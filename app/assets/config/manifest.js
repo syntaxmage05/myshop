3b95ef06b6de12bccdd2ee92f024bf0d48173109
devmuturi <muturinjonge09@gmail.com>
2026-02-22 01:04:55 +0300

    Setup the hoepage

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../builds
 //= link_tree ../builds .css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
